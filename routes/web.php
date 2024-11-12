c0f58de38340ebdf00e8c20d9363963a39c9f783
Anderson Lietz <andersonrenatolietztj@gmail.com>
2024-11-12 11:10:25 -0300

    Aulas 14 - 20 | Controllers, Migrations

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/empresa', function () {
-    return redirect()->route('admin.users');//view('empresa');
-});
-
-Route::group([
-    'prefix'=>'admin',
-    'as'=>'admin.'
-],function(){
+Route::resource('/produtos', ProdutoController::class);
 
 
 
-    Route::get('admin/dashboard', function(){
-        return "dashboard";
-    })->name('dashboard');
+
+
+
+
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// ///-- Controllers
+// Route::get('/',[ProdutoController::class,'index'])->name('produto.index');
+// Route::get('/produto/{id?}',[ProdutoController::class,'show'])->name('produto.show');
+
+// //------------
+// Route::get('/empresa', function () {
+//     return redirect()->route('admin.users');//view('empresa');
+// });
+
+// Route::group([
+//     'prefix'=>'admin',
+//     'as'=>'admin.'
+// ],function(){
+
+//     Route::get('admin/dashboard', function(){
+//         return "dashboard";
+//     })->name('dashboard');
     
-    Route::get('admin/users', function(){
-        return "users";
-    })->name('users');
+//     Route::get('admin/users', function(){
+//         return "users";
+//     })->name('users');
     
-    Route::get('admin/clientes', function(){
-        return "clientes";
-    })->name('clientes');
-});
+//     Route::get('admin/clientes', function(){
+//         return "clientes";
+//     })->name('clientes');
+// });
 
 
 
